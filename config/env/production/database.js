da9ae95914b6a9bb7d9c56f1a4d53de34f256a2b
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 19:28:13 +0200

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,29 +1,21 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+// config/env/production/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password
-      },
-      debug: false,
-      acquireConnectionTimeout: 5000,
-      pool: {
-        min: 0,
-        max: 10,
-        createTimeoutMillis: 8000,
-        acquireTimeoutMillis: 8000,
-        idleTimeoutMillis: 8000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 100
+      host: env('DATABASE_HOST', ''),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false)
       }
     },
-  };
-};
+    pool: {
+      min: 0,
+      max: 5
+    },
+    debug: false
+  }
+});
