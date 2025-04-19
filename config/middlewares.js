40ae64db3c93c7ceabda30a6331251ef8c373a76
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-04-19 19:21:58 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,32 @@
-   // Nell'array di middlewares, cerca la sezione CORS e modifica:
-   {
-     name: 'strapi::cors',
-     config: {
-       origin: ['http://localhost:3000', 'https://kampe-website.vercel.app'],
-       headers: '*',
-     },
-   },
-   
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      frameguard: false,
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: '*',
+      origin: ['http://localhost:3000', 'https://kampe-website.vercel.app']
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
